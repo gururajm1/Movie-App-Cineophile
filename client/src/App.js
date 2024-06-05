1db90794758012ff669035b2b44fa14c71ef62a2
Gururaj2004 <guruguru6631@gmail.com>
2024-06-05 18:51:57 +0530

    production build

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AllMovies from "./pages/AllMovies";
 import Landing from "./pages/Landing";
 import Player from "./pages/Player";
 import Hero from './Hero/Hero'
@@ -14,7 +13,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/player" element={<Player />} />
-        <Route exact path="/allmovies" element={<AllMovies />} />
         <Route exact path="/" element={<Hero />} />
         <Route exact path="/myplaylist" element={<Playlist />} />
         <Route path="/shared-playlist/:email" element={<SharedPlaylist />} />
